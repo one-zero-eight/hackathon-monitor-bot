89ab3a1290a23f94c18f5b49a04cc7ed066ccac1
evermake <vladeryabkin@gmail.com>
2023-11-05 01:37:19 +0300

    refactor: remove unused code

--- src/bot/messages.ts
+++ src/bot/messages.ts
@@ -1,41 +1,5 @@
 import type { Action, Alert } from "@/api"
-import createTextTable from "@/utils/createTextTable"
 import escapeHtml from "@/utils/escapeHtml"
-
-export function pgStatActivitySummary(data: {
-  success: true
-  stats: {
-    processCountByState: {
-      active: number
-      idle: number
-      idleInTransaction: number
-      idleInTransactionAborted: number
-      fastpathFunctionCall: number
-      disabled: number
-      noState: number
-    }
-  }
-} | {
-  success: false
-  message: string
-}): string {
-  if (data.success) {
-    return createTextTable({
-      header: ["State", "Process Count"],
-      body: [
-        ["Active", data.stats.processCountByState.active.toString()],
-        ["Idle", data.stats.processCountByState.idle.toString()],
-        ["Idle TX", data.stats.processCountByState.idleInTransaction.toString()],
-        ["Idle TX Aborted", data.stats.processCountByState.idleInTransactionAborted.toString()],
-        ["Fastpath Func Call", data.stats.processCountByState.fastpathFunctionCall.toString()],
-        ["Disabled", data.stats.processCountByState.disabled.toString()],
-        ["No State", data.stats.processCountByState.noState.toString()],
-      ],
-    })
-  } else {
-    return `❌ Ошибка ❌\n\n${data.message}`
-  }
-}
 
 export function actionDetails(action: Action) {
   return `Действие: <b>${escapeHtml(action.title)}</b>${action.description ? `\n\nОписание: ${escapeHtml(action.description)}` : ""}`
