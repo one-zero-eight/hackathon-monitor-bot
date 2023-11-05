06f4de6aa2e4f81a8880f83cf7d56642f8f022f3
evermake <vladeryabkin@gmail.com>
2023-11-05 05:48:26 +0300

    fix: answer `views` callback query

--- src/bot/middlewares/views.ts
+++ src/bot/middlewares/views.ts
@@ -14,6 +14,7 @@ composer.command("views", async (ctx) => {
 })
 
 composer.callbackQuery("views", async (ctx) => {
+  ctx.answerCallbackQuery()
   await sendViews({
     ctx,
     chatId: ctx.callbackQuery.from.id,
